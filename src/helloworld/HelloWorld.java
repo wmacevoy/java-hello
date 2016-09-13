0128036bf6d6d8a9ab713efbf1b4951285a7a843
Warren MacEvoy <wmacevoy@gmail.com>
2016-09-13 11:11:39 -0600

    test ints

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -60,9 +60,18 @@ public class HelloWorld {
         assert (true || ((1/0) == 3) ) == true;
     }
     
+    void sideEffect(int y) {
+        println(y);
+    }
     void testInts() {
         assert Integer.MAX_VALUE == Math.pow(2,31)-1;
+        assert Integer.MAX_VALUE == 2_147_483_647;
+        assert Integer.MAX_VALUE == 0b0111_1111_1111_1111_1111_1111_1111_1111;
+
         assert Integer.MIN_VALUE == -Math.pow(2,31);
+        assert Integer.MIN_VALUE == -2_147_483_648;
+        assert Integer.MIN_VALUE == 0b1000_0000_0000_0000_0000_0000_0000_0000;
+
         assert Integer.MIN_VALUE - 1 == Integer.MAX_VALUE;
         assert Integer.MAX_VALUE + 1 == Integer.MIN_VALUE;
         
@@ -84,7 +93,73 @@ public class HelloWorld {
         
         assert 17 % 5 == 2;
         assert -7 % 5 == -2;
-        assert -7 % -5 == -2;
+        assert -7 % -5 == -2;       
+        
+        int a = 1;
+        
+        ++a;
+        
+        try (Close out = outExpect("2")) { println(a); }
+        
+        try (Close out = outExpect("3")) { println(++a); }
+        try (Close out = outExpect("3")) { println(a); }
+
+        try (Close out = outExpect("3")) { println(a++); }
+        try (Close out = outExpect("4")) { println(a); }
+
+        // bitwise not
+        assert   ~0b0000_0000_0000_0000_0000_0000_1111_0000 
+               == 0b1111_1111_1111_1111_1111_1111_0000_1111;
+
+        assert                                 ~0b1111_0000 
+               == 0b1111_1111_1111_1111_1111_1111_0000_1111;
+        
+        // bitwise and
+        assert (0b1111_0000 
+             &  0b1010_1010) ==         
+                0b1010_0000;
+
+        // bitwise or
+        assert (0b1111_0000 
+             |  0b1010_1010) ==         
+                0b1111_1010;
+        
+        // bit-shift operator
+        assert (0b1010_1111_0000 >> 4) == 
+                     0b1010_1111;               
+        
+        // bit-shift operator   
+        assert (0b1010_1111_0000 << 4) == 
+           0b1010_1111_0000_0000;               
+
+        int b = random(0,1_000_000);
+
+        assert ((b >> 1) == b/2 );
+        assert (((-b) >> 1) == -b/2 );
+        assert ((b << 1) == 2*b);
+        assert (((-b) << 1) == 2*(-b));
+        
+        int c = random(-1_000_000,1_000_000);
+        
+        assert ((~c)+1) == -c;
+        
+        assert -2 ==        0b1111_1111_1111_1111_1111_1111_1111_1110;
+
+        // right shifts shift the sign bit in (highest order bit)
+        assert (-2 >> 1) == 0b1111_1111_1111_1111_1111_1111_1111_1111;
+    }
+    
+    void testConvert() {
+        byte x = -1;
+        assert x == (byte) 0b1111_1111;
+        int y = x;
+        assert y == -1;
+        assert 0b1111_1111_1111_1111_1111_1111_1111_1111 ==
+                                (int) (byte) 0b1111_1111; 
+    }
+    
+    void testLoop() {
+        
         
     }
 }
